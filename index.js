7f235cd0f3c83ead854378529d9191aa7c9416a1
wfromont <william.fromont@capgemini.com>
2016-06-19 19:12:18 +0200

    Added Open

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var app = require('express')();
 var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var myDroneandler  = require('./lib/droneHandler.js');
+var myDroneandler = require('./lib/droneHandler.js');
+var open = require('open');
 
 var myDrone = new myDroneandler();
 
@@ -14,40 +15,41 @@ app.use('/fonts', express.static(__dirname + '/public/fonts'));
 
 var PORT = 3000;
 
-function Main(){
-    app.get("/",function(req,res){
+function Main() {
+    app.get("/", function (req, res) {
         res.sendFile(path + "index.html");
     });
 
-    app.get("/video/*",function(req,res){
-        myDrone.serveDroneVideo(req,res);
+    app.get("/video/*", function (req, res) {
+        myDrone.serveDroneVideo(req, res);
     });
 
-    http.listen(PORT, function(){
-        console.log('HTTP Server listening on *:'+PORT);
+    http.listen(PORT, function () {
+        console.log('HTTP Server listening on *:' + PORT);
+        open('http://127.0.0.1:' + PORT)
     });
 
-    io.on('connection',function(socket){
+    io.on('connection', function (socket) {
         console.log("User connected");
 
-        socket.on('keyup',function(data){
+        socket.on('keyup', function (data) {
             myDrone.stopMoving(data);
         });
 
-        socket.on('keydown',function(data){
+        socket.on('keydown', function (data) {
             myDrone.move(data);
         });
 
-        socket.on('action',function(data){
+        socket.on('action', function (data) {
             myDrone.action(data);
         });
 
         var oldBatValue = -1;
-        setInterval(function() {
+        setInterval(function () {
             var newBatValue = myDrone.getBattery();
-            if(newBatValue!== oldBatValue){
+            if (newBatValue !== oldBatValue) {
                 oldBatValue = newBatValue;
-                socket.emit('battery',newBatValue);
+                socket.emit('battery', newBatValue);
             }
         }, 1000);
     });
